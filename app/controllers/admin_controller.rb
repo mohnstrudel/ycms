18392560c251e4398defe553f0a105cf896f4a56
Anton Kostin <schnibba@gmail.com>
2017-02-24 19:23:09 +0300

    Add custom breadcrumbs (based on request.original_url).

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,8 +2,18 @@ class AdminController < ApplicationController
 	layout 'admin'
 
 	before_action :set_locale
+	before_action :get_breadcrumbs
 
   	private
+
+  	def get_breadcrumbs
+  		splitted_url = request.original_fullpath.split("/")
+  		# Remove first object
+  		splitted_url.shift
+  		result = splitted_url.map { |element| element.humanize.capitalize }
+  		session[:breadcrumbs] = result
+  		# debug
+  	end
 
 	def set_locale
 		I18n.locale =  params[:locale] || session[:locale] || I18n.default_locale
--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -28,4 +28,31 @@ module AdminHelper
     return send("text_field", field, options)
   end
 
+  def breadcrumbs
+    # Session breadcrumbs is defines in the admin_controller via a before_action filter
+    bc = session[:breadcrumbs]
+    @content = content_tag("h2", bc.last)
+    @content << content_tag(:ol, class: "breadcrumb") do
+        bc.collect do |crumb|
+          if crumb.equal? bc.last
+            content_tag(:li, "<strong>#{crumb}</strong>".html_safe, class: "active")
+          else
+            content_tag(:li, crumb)
+          end
+        end.join.html_safe
+      end
+
+    # End result should look like this:
+    #  %h2 Static Tables
+    #  %ol.breadcrumb
+    #   %li
+    #    %a{:href => "index.html"} Home
+    #   %li
+    #    %a Tables
+    #   %li.active
+    #    %strong Static Tables
+
+    return @content
+  end
+
 end
